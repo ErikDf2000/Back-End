f48e6b3b65baa97d07664aa0142cd8e2cde92b24
usuario <usuario@DESKTOP-T93RB6D>
2021-11-04 13:46:45 -0500

    NombreorApellido

--- src/main/java/com/gym/idat/controller/TrainerController.java
+++ src/main/java/com/gym/idat/controller/TrainerController.java
@@ -3,12 +3,15 @@ package com.gym.idat.controller;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gym.idat.model.Trainers;
@@ -23,22 +26,33 @@ public class TrainerController {
     private TrainerService service;
 
 	 @GetMapping 
-	    public List<Trainers>findAllTrainer(){return service.getTrainers();
+	    public List<Trainers>findAllTrainer(){
+		 return service.getTrainers();
 	    }
 	
 	
     @PostMapping
-    public Trainers agregar(@RequestBody Trainers trainers) { return service.save(trainers);
+    public Trainers agregar(@RequestBody Trainers trainers) { 
+    	return service.save(trainers);
     }
 
    
 
     @GetMapping("/{id}")
-    public Trainers findTrainerById(@PathVariable int id) {return service.getTrainerById(id);
+    public Trainers findTrainerById(@PathVariable int id) {
+    	
+    	return service.getTrainerById(id);
     }
 
     @GetMapping("/{nombre}")
-    public Trainers findTrainerByNombre(@PathVariable String nombre) { return service.getTrainerByNombre(nombre);
+    public Trainers findTrainerByNombre(@PathVariable String nombre) { 
+    	return service.getTrainerByNombre(nombre);
+    }
+    
+    @GetMapping(value = "/buscar")
+    public ResponseEntity<?> findByNombreOrApellido(@RequestParam String dato){
+        List<Trainers> find = service.findByNombreOrApellido(dato, dato);
+        return ResponseEntity.status(HttpStatus.OK).body(find);
     }
 
     @PutMapping("/actualizar")
