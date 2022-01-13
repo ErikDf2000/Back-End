fe1ff6d0bb4883228db7a778ad400568628ba6e8
usuario <usuario@DESKTOP-T93RB6D>
2022-01-13 03:54:51 -0500

    transac

--- src/main/java/com/gym/idat/controller/ReservaController.java
+++ src/main/java/com/gym/idat/controller/ReservaController.java
@@ -6,6 +6,7 @@ import com.gym.idat.services.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,13 +36,14 @@ public class ReservaController {
     }
     
     
-    @TransactionalEventListener
+    @Transactional(readOnly=true)
     @GetMapping("/filtroCliente")
     public ResponseEntity<?> FiltroCliente(@RequestParam String dato){
     	List<Reserva> find = service.FiltroReserva(dato, dato);
         return ResponseEntity.status(HttpStatus.OK).body(find);
     }
     
+    @Transactional(readOnly=true)
     @GetMapping("/buscarReservaBy/{id}")
     public ResponseEntity<?> SearchReservaByClienteId(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(service.findReservaForIdCliente(id));
